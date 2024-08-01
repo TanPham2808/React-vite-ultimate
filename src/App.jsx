ad1261f6538d3f72b91077fcd41a9e326f3e55c4
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-01 22:16:58 +0700

    Video 34 - Props: Truyền data từ cha sang con (1)

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,22 @@ import TodoNew from './components/todo/TodoNew'
 import reactLogo from './assets/react.svg'
 
 const App = () => {
+  const hoidanIT = "TanPham";
+  const age = 25;
+  const data = {
+    address: "233 Hoang Hoa Tham",
+    country: "Viet Nam"
+  }
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={hoidanIT}
+        age={age}
+        data={data}
+      />
       <div className='todo-image'>
         <img src={reactLogo} className='logo'></img>
       </div>
--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,6 +1,19 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    console.log(">>>> check props:", props)
+    // Props là 1 object
+    // {
+    //     name = "TanPham";
+    //     age = 25;
+    //     data = {
+    //       address: "233 Hoang Hoa Tham",
+    //       country: "Viet Nam"
+    //     }
+    // }
+    const { name, age, data } = props;
+
     return (
         <div className='todo-data'>
+            <div>My name is {name}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
