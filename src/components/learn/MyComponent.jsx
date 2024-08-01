1065da08797e9515ed8bdbbe2fba74b165c6294a
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-01 20:52:40 +0700

    video 31: Sử dụng biến với JSX

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,17 @@
 import './style.css';
 
 const MyComponent = () => {
+    const a = 'test'
+    const b = 123
+    const objectTan = {
+        ten: "Pham Xuan Tan",
+        tuoi: 25
+    }
+
     return (
         <>
-            <div className='child'>Ta là Component</div>
+            <div className='child'>Ta là Component {objectTan.ten}</div>
+            <div className='child'>Ta là Component {JSON.stringify(objectTan)}</div>
         </>
     );
 }
