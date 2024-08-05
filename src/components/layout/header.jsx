c7bf9b55c1fbad9f386e6a2ce7758b89b9f38a2a
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-05 23:44:09 +0700

    Video 50: Sử dụng Navlink để thêm active cho menu

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './header.css'
 
 const Header = () => {
     return (
         <ul>
-            <li><Link class="active" to="/">Home</Link></li>
-            <li><Link to="/user">Users</Link></li>
-            <li><Link to="/products">About</Link></li>
+            <li><NavLink to="/">Home</NavLink></li>
+            <li><NavLink to="/user">Users</NavLink></li>
+            <li><NavLink to="/products">About</NavLink></li>
         </ul>
     );
 }
