6dd1e69c79b3379f813381550e87c1ecdd0147b4
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-05 23:25:25 +0700

    Video 47: Tạo Header & Footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/TodoData'
 import TodoNew from './components/todo/TodoNew'
 import reactLogo from './assets/react.svg'
 import { useState } from 'react'
+import Header from './components/layout/header'
+import Footer from './components/layout/footer'
 
 const App = () => {
   const [todoList, setTodoList] = useState([])
@@ -27,21 +29,25 @@ const App = () => {
 
 
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew
-        addNewTodo={addNewTodo}
-      />
+    <>
+      <Header />
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew
+          addNewTodo={addNewTodo}
+        />
 
-      {todoList.length > 0 ?
-        <TodoData todoList={todoList} deleteTodo={deleteTodo} />
-        :
-        <div className='todo-image'>
-          <img src={reactLogo} className='logo'></img>
-        </div>
-      }
+        {todoList.length > 0 ?
+          <TodoData todoList={todoList} deleteTodo={deleteTodo} />
+          :
+          <div className='todo-image'>
+            <img src={reactLogo} className='logo'></img>
+          </div>
+        }
 
-    </div>
+      </div>
+      <Footer />
+    </>
   )
 }
 
---
+++ src/components/layout/footer.jsx
@@ -0,0 +1,11 @@
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <div class="footer">
+            <p>React Vite - @Tan Pham</p>
+        </div>
+    );
+}
+
+export default Footer;
