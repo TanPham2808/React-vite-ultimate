e8a3c80f29885ad5ccf4a1830ac619a0465d2fb7
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-05 23:40:36 +0700

    Video 49: Không load page bằng cách sử dụng Link của React router dom

--- src/components/layout/header.jsx
+++ src/components/layout/header.jsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import './header.css'
 
 const Header = () => {
     return (
         <ul>
-            <li><a class="active" href="/">Home</a></li>
-            <li><a href="/user">Users</a></li>
-            <li><a href="/products">About</a></li>
+            <li><Link class="active" to="/">Home</Link></li>
+            <li><Link to="/user">Users</Link></li>
+            <li><Link to="/products">About</Link></li>
         </ul>
     );
 }
