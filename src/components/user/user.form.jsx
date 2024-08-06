6abf8fc01335c3bdb3b326e49c0fea5e65ef4d94
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-06 21:36:00 +0700

    Video 67: State hóa form

---
+++ src/components/user/user.form.jsx
@@ -0,0 +1,62 @@
+import { Input } from 'antd';
+import { Button, Flex } from 'antd';
+import { useState } from 'react';
+
+const UserForm = () => {
+    const [fullName, setFullName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+
+    const handleCreateUser = () => {
+        const data = {
+            fullName: fullName,
+            email: email,
+            password: password,
+            phoneNumber: phoneNumber
+        }
+
+        console.log(">>>>check data", data)
+    }
+
+    return (
+
+        <div className="user-form" style={{ margin: "20px 0" }}>
+            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                <div>
+                    <span>FullName</span>
+                    <Input
+                        value={fullName}
+                        onChange={(event) => setFullName(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <span>Email</span>
+                    <Input
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <span>Password</span>
+                    <Input.Password
+                        value={password}
+                        onChange={(event) => setPassword(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <span>Phone number</span>
+                    <Input
+                        value={phoneNumber}
+                        onChange={(event) => setPhoneNumber(event.target.value)}
+                    />
+                </div>
+                <div>
+                    <Button type="primary" onClick={handleCreateUser}>Create User</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default UserForm;
