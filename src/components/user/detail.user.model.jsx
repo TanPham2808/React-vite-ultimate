429598bf2bf4c1e1d006c9f9d86a9a6c31592237
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-10 16:29:34 +0700

    video 82: Thêm avarta cho user

--- src/components/user/detail.user.model.jsx
+++ src/components/user/detail.user.model.jsx
@@ -1,9 +1,8 @@
-import { Input } from 'antd';
+import { Input, Button } from 'antd';
 import { Drawer } from 'antd';
 
 const DetailUserModel = (props) => {
     const { isModalDetailOpen, setIsModalDetailOpen, dataDetail, setDataDetail } = props;
-
     return (
         <Drawer title="Detail User"
             onClose={() => {
@@ -13,6 +12,25 @@ const DetailUserModel = (props) => {
             open={isModalDetailOpen}>
             {dataDetail ? <>
                 <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                    <div>
+                        <img
+                            height={200}
+                            width={200}
+                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                    </div>
+                    <div>
+                        <label htmlFor='btnUpload'
+                            style={{
+                                width: "fit-content",
+                                background: "orange",
+                                marginTop: "15px",
+                                padding: "5px 10px",
+                                borderRadius: "5px",
+                                cursor: "pointer"
+                            }}>Upload Avatar
+                        </label>
+                        <input id='btnUpload' type='file' hidden />
+                    </div>
                     <div>
                         <span>Full Name</span>
                         <Input
