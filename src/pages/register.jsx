401eef7dd3d9551262b537e6355f31cd1dfc811a
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-13 22:42:10 +0700

    Video 89: Sử dụng Uncontroll Component cho Register (rất ha)y

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,6 +1,78 @@
+import { Input, Button, Form } from 'antd';
+import { useState } from 'react';
+
 const RegisterPage = () => {
+    const [form] = Form.useForm();
+    const onFinish = (values) => {
+        console.log("check value", values);
+    }
+
     return (
-        <div>Register Page</div>
+        <Form
+            layout="vertical"
+            form={form}
+            onFinish={onFinish}
+        // onFinishFailed={onFinishFailed}
+        >
+            <div style={{ margin: "50px", }}>
+                <Form.Item
+                    label="Full Name"
+                    name="fullName"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Email"
+                    name="email"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+
+                <Form.Item
+                    label="Password"
+                    name="password"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input.Password />
+                </Form.Item>
+
+                <Form.Item
+                    label="Phone Number"
+                    name="phone"
+                // rules={[
+                //     {
+                //         required: true,
+                //         message: 'Please input your username!',
+                //     },
+                // ]}
+                >
+                    <Input />
+                </Form.Item>
+                <div>
+                    <Button onClick={() => form.submit()}
+                        type="primary">Register
+                    </Button>
+                </div>
+            </div>
+        </Form>
     )
 }
 
