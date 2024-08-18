ebca39a381c3118a9f74f337f8d69f2c0f15b4f6
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-08-18 19:26:39 +0700

    Video 112: Thêm Loading Bar (giống youtube)

--- src/services/axios.customize.js
+++ src/services/axios.customize.js
@@ -1,4 +1,11 @@
 import axios from "axios";
+import NProgress from 'nprogress'
+
+NProgress.configure({
+    showSpinner: false,
+    trickleSpeed: 100,
+});
+
 
 // Set config defaults when creating the instance
 const instance = axios.create({
@@ -9,6 +16,7 @@ const instance = axios.create({
 //instance.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 
 instance.interceptors.request.use(function (config) {
+    NProgress.start();
     if (typeof window !== "undefined" && window && window.localStorage &&
         window.localStorage.getItem('access_token')) {
         config.headers.Authorization = 'Bearer ' + window.localStorage.getItem('access_token');
@@ -16,12 +24,15 @@ instance.interceptors.request.use(function (config) {
     // Do something before request is sent
     return config;
 }, function (error) {
+    NProgress.done();
     // Do something with request error
     return Promise.reject(error);
 });
 
 // Add a response interceptor
 instance.interceptors.response.use(function (response) {
+    NProgress.done();
+
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
     if (response.data && response.data.data) {
@@ -29,6 +40,8 @@ instance.interceptors.response.use(function (response) {
     }
     return response;
 }, function (error) {
+    NProgress.done();
+
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     if (error.response && error.response.data) {
